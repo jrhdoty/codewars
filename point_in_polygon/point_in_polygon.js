5f5032d42fa74706e272c161d88ef91d9873d6fc
John Doty <jrhdoty@gmail.com>
2014-10-20 16:02:38 -0700

    add ray casting solution for point in a polygon problem

--- point_in_polygon/point_in_polygon.js
+++ point_in_polygon/point_in_polygon.js
@@ -16,6 +16,66 @@ In the tests, the point will never fall exactly on an edge of the polygon.
 */
 
 //Return true if point is inside poly, and false if it is not
+
+//let's try ray casting
 function pointInPoly(poly, point) {
+  var count = 0;
+  for (var i = 0; i < poly.length; i++){
+    var startSide = poly[i];
+    var endSide = poly[(i+1)%poly.length];
+    if ( rayIntersection(point, startSide, endSide) ){
+      count++;
+    } else{
+    }
+  }
+  if (count % 2 === 0){
+    return false;
+  }
+  return true;
+}
+
+function rayIntersection(point, start, end){
+  var minY, maxY, shifted;
+  if (start[1] < end[1]){
+    minY = start;
+    maxY = end;
+  } else {
+    maxY = start;
+    minY = end;  
+  }
+
+  //avoid issue of intersection with a vertex
+  if (point[1] === minY[1] || point[1] === maxY[1]){
+    shifted = [point[0], point[1]];
+    shifted[1]+= 0.000001;
+    point = shifted;
+  }
+
+  //if above max point or below min point, no intersection
+  if (point[1] > maxY[1] || point[1] < minY[1]) {
+    return false;
+  }
+
+  //if to the left of min point then intersection
+  if (point[0] < Math.min(start[0], end[0])) {
+    return true;
+  }
+
+  //if to the right of max point, then no intersection
+  if (point[0] > Math.max(start[0], end[0])) {
+    return false;
+  }
+
+  //if slope from bottom point to target is less than
+  //slope of bottom point to top point, then intersection
+  var pointSlope = point[0] !== minY[0] ? (point[1]-minY[1])/(point[0]-minY[0]) : 0;
+  var edgeSlope  = maxY[0] !== minY[0] ? (maxY[1]-minY[1])/(maxY[0]-minY[0]) : 0;
+
+  if ( pointSlope > edgeSlope ){
+    return  true;
+  }
   return false;
 }
+
+
+
